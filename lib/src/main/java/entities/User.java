fe156c58ef9d7c707295dbd8082aabf3ab615666
Sahil Khilari <sahilkhilari16@gmail.com>
2024-12-23 00:12:47 +0530

    Gradle Issue Fixed

--- lib/src/main/java/entities/User.java
+++ lib/src/main/java/entities/User.java
@@ -1,5 +1,7 @@
 package entities;
 
+import java.util.List;
+
 public class User {
 
     private String name;
--- lib/src/main/java/services/UserBookingService.java
+++ lib/src/main/java/services/UserBookingService.java
@@ -1,14 +1,27 @@
 package services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import entities.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class UserBookingService {
 
     private User user;
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private static final String USERS_PATH = "../localDb/users.json";
+    private List<User> userList;
 
 
-    public UserBookingService(User user) {
+    public UserBookingService(User user1) throws IOException {
+        this.user = user1;
+        File users = new File(USERS_PATH);
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {
+        });
+
     }
-
 
 }
